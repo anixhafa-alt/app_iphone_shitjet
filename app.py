c6f6898f0de3535d2c7dbbbdb69ef5b4847f1dca
anixhafa-alt <anixhafa@gmail.com>
2026-05-16 10:08:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -221,7 +221,7 @@ if not df_raw.empty:
 
         if pd.notnull(data_maksimale):
             # Formati i ri përfshin Datën dhe Orën (%d/%m/%Y %H:%M)
-            koha_formatuar = data_maksimale.strftime("%d/%m/%Y  %H:%M:%S")
+            koha_formatuar = data_maksimale.strftime("%d/%m/%Y në %H:%M:%S")
 
             if data_maksimale.date() == sot_data:
                 st.sidebar.success(f"🟢 Lidhja SQL: LIVE\n\n{koha_formatuar}")
