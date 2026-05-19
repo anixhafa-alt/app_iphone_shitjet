f8720c05c1d14386d83efe56b3cb90e50b4823cb
anixhafa-alt <anixhafa@gmail.com>
2026-05-19 19:08:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ st.sidebar.markdown(
         font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
         font-size: 12px !important;
         color: #566573 !important;
-        text-align: "   "center;
+        text-align: "   " left;
         margin-top: -60px !important;
         margin-bottom: 20px !important;
         letter-spacing: 1px;
